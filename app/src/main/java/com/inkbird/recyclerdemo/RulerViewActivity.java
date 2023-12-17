f714e0ee1070b092a58cfbdf396bbb6bc70951fa
wzh <124529515@qq.com>
2023-12-17 16:46:37 +0800

    wzh

--- app/src/main/java/com/inkbird/recyclerdemo/RulerViewActivity.java
+++ app/src/main/java/com/inkbird/recyclerdemo/RulerViewActivity.java
@@ -7,15 +7,10 @@ import android.os.Bundle;
 import com.inkbird.recyclerdemo.ruler.RulerView;
 
 public class RulerViewActivity extends AppCompatActivity {
-    private RulerView rulerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ruler_view);
-
-        rulerView = findViewById(R.id.ruler_view);
-
-        rulerView.setScope(0, 300, 5, "℉");
     }
 }
